98de2c3058b2113dc9906d2b1cf6b732e4567f3f
Ben Richardson <1033411@student.sae.edu.au>
2025-02-09 14:24:00 +0800

    Initial set up of Interfaces

---
+++ Assets/Scripts/Week Two/Inheritance/SmartEnemy.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SAE.GAD176.Tutorials.Interfaces
+{
+    public class SmartEnemy : FastEnemy
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            playerReference = FindAnyObjectByType<Player>();
+            RunAtPlayer();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            HitPlayer();
+        }
+
+        protected void HitPlayer()
+        {
+            if (playerReference)
+            {
+                if (Vector3.Distance(transform.position, playerReference.transform.position) < 1)
+                {
+                    Debug.Log("Dealing Damage to the player!");
+                }
+            }
+        }
+    }
+}
+
